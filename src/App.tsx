d3ef62d3cee763039ec3c68cc03ac9ce3a45b494
galobponce <personal.galo.bello@gmail.com>
2022-02-24 23:28:30 -0300

    #3 - Register Page - Visual

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
 import routes from './routes';
-import { Login } from './pages';
+import { Login, Register } from './pages';
 import theme from './config/chakra.config';
 import { AuthProvider } from './context/Auth/AuthProvider';
 
@@ -15,11 +15,12 @@ const App: FC = () => {
           <Routes>
             <Route path={routes.DEFAULT} element={<Navigate to={routes.LOGIN} />} />
             <Route path={routes.LOGIN} element={<Login />} />
+            <Route path={routes.REGISTER} element={<Register />} />
           </Routes>
         </Router>
       </AuthProvider>
     </ChakraProvider>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Auth/Register/Register.tsx
@@ -0,0 +1,57 @@
+import { FC, FormEvent, useEffect } from 'react';
+import { Button } from '@chakra-ui/react';
+
+import routes from '../../../routes';
+import { useForm } from '../../../hooks/useForm';
+import { Brand, FormInput } from '../../../common';
+import CardFooterButtons from '../CardFooterButtons';
+import { useAuthContext } from '../../../hooks/useAuthContext';
+import { Container, Card, CardTitle, CardBody, ButtonContainer } from '../styles';
+
+const Register: FC = () => {
+  const { logOut } = useAuthContext();
+
+  const [formValues, onInputChange] = useForm({
+    email: '',
+    password: '',
+    repeatedPassword: ''
+  });
+  const { email, password, repeatedPassword } = formValues;
+
+  useEffect(() => {
+    logOut();
+  }, []);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      // TODO: Register
+    } catch(err: any) {
+      // TODO: Error alert
+    } finally {
+    }
+  };
+
+  return (
+    <Container>
+      <Brand withSlogan/>
+      <Card>
+        <CardTitle>Sign Up</CardTitle>
+        <CardBody>
+          <form onSubmit={handleSubmit}>
+            <FormInput required type='email' name='email' label='Email Address' inputValue={email} onInputChange={onInputChange} />
+            <FormInput required type='password' name='password' label='Password' inputValue={password} onInputChange={onInputChange} />
+            <FormInput required type='password' name='repeatedPassword' label='Re-enter password' inputValue={repeatedPassword} onInputChange={onInputChange} />
+            <ButtonContainer>
+              <Button type='submit' colorScheme='teal' size='lg'>Sign Up</Button>
+            </ButtonContainer>
+          </form>
+        </CardBody>
+        <CardFooterButtons currentUrl={routes.REGISTER} />
+      </Card>
+    </Container>
+  );
+};
+
+export default Register;
